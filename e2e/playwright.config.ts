f1e38a4c78ce36ac7162de9547a697ab903d95d2
Subhav Gautam <9058689+zubhav@users.noreply.github.com.>
2025-07-31 12:49:04 +0100

    Add Playwright test artifacts uploading and CI failure check

--- e2e/playwright.config.ts
+++ e2e/playwright.config.ts
@@ -14,14 +14,16 @@ export default defineConfig({
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: 'list',
+  reporter: process.env.CI
+    ? [['list'], ['html', { outputFolder: 'playwright-report', open: 'never' }], ['github']]
+    : [['list'], ['html', { outputFolder: 'playwright-report', open: 'never' }]],
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
     baseURL: 'http://localhost:6006',
 
-    /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
-    trace: 'on-first-retry',
+    /* Collect trace when retrying the failed test or on CI for all tests. */
+    trace: process.env.CI ? 'retain-on-failure' : 'on-first-retry',
 
     /* Record video for all tests */
     video: 'retain-on-failure',
