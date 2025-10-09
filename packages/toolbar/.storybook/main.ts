5b59e0a01fc21cee0a6aa0a699110da23e345f7f
Cody Spath <cspath@launchdarkly.com>
2025-10-09 11:38:14 -0400

    fix: slightly update storybook (wip)

--- packages/toolbar/.storybook/main.ts
+++ packages/toolbar/.storybook/main.ts
@@ -10,7 +10,11 @@ const config: StorybookConfig = {
   ],
   framework: {
     name: '@storybook/react-vite',
-    options: {},
+    options: {
+      builder: {
+        viteConfigPath: './vite.config.core.ts'
+      }
+    },
   },
   core: {
     disableTelemetry: true,
