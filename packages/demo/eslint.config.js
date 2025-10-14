adb75ff32f4acbadbdf4bffd4ddda897dad755b6
Cody Spath <cspath@launchdarkly.com>
2025-10-14 14:39:51 -0400

    refactor: use defineConfig

--- packages/demo/eslint.config.js
+++ packages/demo/eslint.config.js
@@ -3,12 +3,11 @@ import globals from 'globals';
 import reactHooks from 'eslint-plugin-react-hooks';
 import reactRefresh from 'eslint-plugin-react-refresh';
 import tseslint from 'typescript-eslint';
-import { globalIgnores } from 'eslint/config';
+import { defineConfig } from 'eslint/config';
 
-export default tseslint.config([
-  globalIgnores(['dist']),
-  {
-    files: ['**/*.{ts,tsx}'],
+export default defineConfig({
+  ignores: ['dist'],
+  files: ['**/*.{ts,tsx}'],
     extends: [
       js.configs.recommended,
       tseslint.configs.recommended,
@@ -19,5 +18,4 @@ export default tseslint.config([
       ecmaVersion: 2020,
       globals: globals.browser,
     },
-  },
-]);
+});
