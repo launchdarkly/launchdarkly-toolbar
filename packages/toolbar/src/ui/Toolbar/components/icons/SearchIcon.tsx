66fdb57b17fce54b65d25f7f1b4eb6f031382aef
pranjal-jately-ld <pjately@launchdarkly.com>
2025-10-17 13:37:35 +0100

    fix: update toolbar icons to match launchpad icons (#199)

--- packages/toolbar/src/ui/Toolbar/components/icons/SearchIcon.tsx
+++ packages/toolbar/src/ui/Toolbar/components/icons/SearchIcon.tsx
@@ -9,10 +9,14 @@ export function SearchIcon({ className }: IconProps) {
     <svg
       className={`${styles.icon} ${className}`}
       fill="currentColor"
-      viewBox="0 0 24 24"
+      viewBox="0 0 20 20"
       xmlns="http://www.w3.org/2000/svg"
     >
-      <path d="M10 2a8 8 0 1 0 5.29 14.29l4.3 4.3a1 1 0 0 0 1.42-1.42l-4.3-4.3A8 8 0 0 0 10 2zm0 2a6 6 0 1 1 0 12 6 6 0 0 1 0-12z" />
+      <path
+        fillRule="evenodd"
+        d="M5.814 2.251a6.583 6.583 0 0 1 7.675 10.177l4.542 4.542a.75.75 0 1 1-1.06 1.06l-4.543-4.542a6.58 6.58 0 0 1-8.75-.5A6.585 6.585 0 0 1 5.815 2.252m2.52.999a5.083 5.083 0 1 0 0 10.166 5.083 5.083 0 0 0 0-10.166"
+        clipRule="evenodd"
+      />
     </svg>
   );
 }
