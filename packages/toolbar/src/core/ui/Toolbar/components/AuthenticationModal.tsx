de3d5fdbda242df998fce783b3d9401ff1e5552b
Cody Spath <cspath@launchdarkly.com>
2025-11-07 14:55:36 -0500

    feat: pass window.location.origin as a query param to enforce strict postMessage comms

--- packages/toolbar/src/core/ui/Toolbar/components/AuthenticationModal.tsx
+++ packages/toolbar/src/core/ui/Toolbar/components/AuthenticationModal.tsx
@@ -20,7 +20,7 @@ export function AuthenticationModal(props: AuthenticationModalProps) {
   const getIframeUrl = useCallback(() => {
     switch (baseUrl.toLowerCase()) {
       case 'https://app.launchdarkly.com':
-      return 'https://integrations.launchdarkly.com'
+        return 'https://integrations.launchdarkly.com'
       case 'https://ld-stg.launchdarkly.com':
         return 'https://integrations-stg.launchdarkly.com';
       case 'https://app.ld.catamorphic.com':
@@ -70,7 +70,7 @@ export function AuthenticationModal(props: AuthenticationModalProps) {
   return (
     <div className={styles.iframeContainer}>
       <iframe
-        src={authenticating ? `${getIframeUrl()}/toolbar/authenticating.html` : `${getIframeUrl()}/toolbar/index.html`}
+        src={authenticating ? `${getIframeUrl()}/toolbar/authenticating.html?originUrl=${window.location.origin}` : `${getIframeUrl()}/toolbar/index.html?originUrl=${window.location.origin}`}
         title="LaunchDarkly Toolbar"
         style={{ display: 'none' }}
         ref={ref}
