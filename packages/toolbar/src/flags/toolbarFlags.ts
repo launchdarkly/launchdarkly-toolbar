87b5f39badd651791affda82c063ab7a48aa073c
Pranjal Jately <pjately@launchdarkly.com>
2025-11-15 19:05:20 +0000

    refactor: remove unused internal client test flag and clean up ActionButtons

--- packages/toolbar/src/flags/toolbarFlags.ts
+++ packages/toolbar/src/flags/toolbarFlags.ts
@@ -1,3 +1,10 @@
 import { createToolbarFlagFunction } from './createToolbarFlagFunction';
 
-export const internalClientTest = createToolbarFlagFunction('internal-client-test', true);
+/**
+ *
+ * These flags control features within the toolbar itself using the toolbar's
+ * internal LaunchDarkly client (not the user's application client).
+ *
+ * To add a toolbar feature flag, export it here like:
+ * export const myFeature = createToolbarFlagFunction('my-feature-key', defaultValue);
+ */
