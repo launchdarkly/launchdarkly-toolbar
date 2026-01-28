e5c538903f9222695d9f20d9cc2c1e4390120626
Steven Zhang <szhang@launchdarkly.com>
2026-01-28 14:42:34 -0600

    fix: typing to align with browser sdk v4

--- packages/toolbar/src/core/utils/analytics.ts
+++ packages/toolbar/src/core/utils/analytics.ts
@@ -1,4 +1,4 @@
-import type { LDClient } from 'launchdarkly-js-client-sdk';
+import type { LDClient } from '../../types/plugins/LDClient';
 import type { FeedbackSentiment } from '../../types/analytics';
 import { isDoNotTrackEnabled } from './browser';
 import { sendFeedback } from './feedback';
